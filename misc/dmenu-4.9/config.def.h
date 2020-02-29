5c8a4a28b55b2f0adda9021bfa6ece21c45d1a45
simon <aracus755@gmail.com>
2020-02-29 18:45:20 +0900

    Added some patches yet some are not working

--- misc/dmenu-4.9/config.def.h
+++ misc/dmenu-4.9/config.def.h
@@ -2,15 +2,17 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 300;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"hack:size=13"
+	"Hack:size=16"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#e85d44", "#6a5141" },
+	[SchemeNorm] = { "#4f8ec8", "#fbf8ef" },
+	[SchemeSel] = { "#77416e", "#9888b6" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -21,3 +23,6 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 5;
