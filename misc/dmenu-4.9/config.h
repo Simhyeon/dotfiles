89f0d4b0cdde4aea3f342de674dcb38dae3378cf
육심현 <simoncreek@tutanota.com>
2020-05-13 00:22:01 +0900

    Changed color scheme

--- misc/dmenu-4.9/config.h
+++ misc/dmenu-4.9/config.h
@@ -11,8 +11,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#a89984", "#282828" },
-	[SchemeSel] = { "#282828", "#a89984" },
+	[SchemeNorm] = { "#d0d0d0", "#3a3a3a" },
+	[SchemeSel] = { "#3a3a3a", "#d0d0d0" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
