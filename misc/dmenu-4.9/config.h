17e36480bb4c0c4c83ab8487831c1b57c81d762f
simoncreek <simoncreek@tutanota.com>
2020-07-10 03:21:42 +0900

    New dmenu

--- misc/dmenu-4.9/config.h
+++ misc/dmenu-4.9/config.h
@@ -6,7 +6,7 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 300;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack:size=16"
+	"D2Coding:size=20"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -25,4 +25,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 0;
+static unsigned int border_width = 1;
